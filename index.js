e5982b8ebaf5f454a3ed226d7786abf3a2841034
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-05-09 23:38:48 -0400

    Create array of single words using array map method.

--- index.js
+++ index.js
@@ -11,22 +11,34 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return 
-  
-  // return new array with case tutorial names ... use map()
-const newtutorials = tutorials.map()
-  
-  // access each word in turtorials --- how to sperate 
+// const titleCased = () => {
+//   return tutorials.map(function(line)) {
+//   }
+// }
 
-  // iteration second time ... what data is here?
-
-  // capitalize first letter of each word
-  // function that takes word first letter capitlaize it and concatenate with slice
-
-function capitalizeFirstLetter(word) {
-  return word.charAt(0).uppercase() + word.slice(1);
+function titleCased() {
+  return tutorials.map(function(line) {
+    const lineChunks = line.split(" ");
+    console.log("Chunks:", lineChunks);
+  });
 }
 
+titleCased()
 
-}
+  
+//   // return new array with case tutorial names ... use map()
+// const newtutorials = tutorials.map(f())
+  
+//   // access each word in turtorials --- how to sperate ... splite (" ")
+//   //feed tutorial lines info function that creates small chucks
+
+// const lineChunks = line.split(" ");
+//   // iteration second time ... what data is here?
+
+//   // capitalize first letter of each word
+//   // function that takes word first letter capitlaize it and concatenate with slice
+
+// function capitalizeFirstLetter(word) {
+//   return word.charAt(0).uppercase() + word.slice(1);
+
+//  const orignalLine = "what does the this keyword mean?"
